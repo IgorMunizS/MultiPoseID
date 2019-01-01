c9bd8ee1f09f0e0228cf6778a932bbae28b44be8
IgorMunizS <by_skall@hotmail.com>
2019-01-01 13:39:59 -0200

    change path generator to load images

--- utils/retinanet/preprocessing/coco.py
+++ utils/retinanet/preprocessing/coco.py
@@ -116,7 +116,7 @@ class CocoGenerator(Generator):
         """ Load an image at the image_index.
         """
         image_info = self.coco.loadImgs(self.image_ids[image_index])[0]
-        path       = os.path.join(self.data_dir, 'images', self.set_name, image_info['file_name'])
+        path       = os.path.join(self.data_dir, self.set_name, image_info['file_name'])
         return read_image_bgr(path)
 
     def load_annotations(self, image_index):
