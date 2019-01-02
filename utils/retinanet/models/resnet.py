6d91f08e5557c087e4d9af8073d6dd211ecd0c2a
IgorMunizS <by_skall@hotmail.com>
2019-01-02 11:23:10 -0200

    retire preprocess image to match the same approach of keypoint net

--- utils/retinanet/models/resnet.py
+++ utils/retinanet/models/resnet.py
@@ -70,7 +70,8 @@ class ResNetBackbone(Backbone):
     def preprocess_image(self, inputs):
         """ Takes as input an image and prepares it for being passed through the network.
         """
-        return preprocess_image(inputs, mode='tf')
+        #return preprocess_image(inputs, mode='tf')
+        return inputs
 
 
 def resnet_retinanet(num_classes, backbone='resnet50', inputs=None, modifier=None, **kwargs):
