9dfdc37d93f0c022d278660a842a29934a5bf38c
IgorMunizS <by_skall@hotmail.com>
2019-01-06 09:55:43 -0200

    add syspath to train prnnet

--- training/train_prnnet.py
+++ training/train_prnnet.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 import keras
 from pycocotools.coco import  COCO
 from network.prn_network import PRN, PRN_Seperate
