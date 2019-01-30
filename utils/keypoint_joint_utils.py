610e716d9297ca4f9266b916089138edbef8051a
Igor Muniz <igor.muniz.ims@gmail.com>
2019-01-30 14:05:27 -0200

    joint utils to plot bounding box and keypoints

---
+++ utils/keypoint_joint_utils.py
@@ -0,0 +1,213 @@
+import cv2
+import math
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter, maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure
+
+# Color code used to plot different joints and limbs (eg: joint_type=3 and
+# limb_type=3 will use colors[3])
+# colors = [
+#     [255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0],
+#     [85, 255, 0], [0, 255, 0], [0, 255, 85], [0, 255, 170], [0, 255, 255],
+#     [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], [170, 0, 255],
+#     [255, 0, 255], [255, 0, 170], [255, 0, 85], [255, 0, 0]]
+
+colors = [
+    [255, 0, 0], [255, 0, 0], [255, 0, 255], [255, 0, 0], [255, 0, 0],
+    [255, 0, 0], [255, 0, 0], [255, 0, 0], [255, 0, 0], [255, 0, 0],
+    [255, 0, 0], [255, 0, 0], [255, 0, 0], [255, 0, 0], [255, 0, 0],
+    [255, 0, 0], [255, 0, 0], [255, 0, 0]]
+
+limbSeq = [[0,1], [1,2], [2,3], [0,4], [4,5], [5,6], [0,7], [7,8], [8,9], [0,10], [10,11], [11,12], \
+[0,13], [13,15], [0,14],[14,16]]
+
+
+NUM_JOINTS = 18
+
+
+def find_peaks(param, img):
+    """
+    Given a (grayscale) image, find local maxima whose value is above a given
+    threshold (param['thre1'])
+    :param img: Input image (2d array) where we want to find peaks
+    :return: 2d np.array containing the [x,y] coordinates of each peak found
+    in the image
+    """
+
+    peaks_binary = (maximum_filter(img, footprint=generate_binary_structure(
+        2, 1)) == img) * (img > param['thre1'])
+    # Note reverse ([::-1]): we return [[x y], [x y]...] instead of [[y x], [y
+    # x]...]
+    return np.array(np.nonzero(peaks_binary)[::-1]).T
+
+
+def compute_resized_coords(coords, resizeFactor):
+    """
+    Given the index/coordinates of a cell in some input array (e.g. image),
+    provides the new coordinates if that array was resized by making it
+    resizeFactor times bigger.
+    E.g.: image of size 3x3 is resized to 6x6 (resizeFactor=2), we'd like to
+    know the new coordinates of cell [1,2] -> Function would return [2.5,4.5]
+    :param coords: Coordinates (indices) of a cell in some input array
+    :param resizeFactor: Resize coefficient = shape_dest/shape_source. E.g.:
+    resizeFactor=2 means the destination array is twice as big as the
+    original one
+    :return: Coordinates in an array of size
+    shape_dest=resizeFactor*shape_source, expressing the array indices of the
+    closest point to 'coords' if an image of size shape_source was resized to
+    shape_dest
+    """
+
+    # 1) Add 0.5 to coords to get coordinates of center of the pixel (e.g.
+    # index [0,0] represents the pixel at location [0.5,0.5])
+    # 2) Transform those coordinates to shape_dest, by multiplying by resizeFactor
+    # 3) That number represents the location of the pixel center in the new array,
+    # so subtract 0.5 to get coordinates of the array index/indices (revert
+    # step 1)
+    return (np.array(coords, dtype=float) + 0.5) * resizeFactor - 0.5
+
+
+def NMS(param, heatmaps, upsampFactor=1., bool_refine_center=True, bool_gaussian_filt=False):
+    """
+    NonMaximaSuppression: find peaks (local maxima) in a set of grayscale images
+    :param heatmaps: set of grayscale images on which to find local maxima (3d np.array,
+    with dimensions image_height x image_width x num_heatmaps)
+    :param upsampFactor: Size ratio between CPM heatmap output and the input image size.
+    Eg: upsampFactor=16 if original image was 480x640 and heatmaps are 30x40xN
+    :param bool_refine_center: Flag indicating whether:
+     - False: Simply return the low-res peak found upscaled by upsampFactor (subject to grid-snap)
+     - True: (Recommended, very accurate) Upsample a small patch around each low-res peak and
+     fine-tune the location of the peak at the resolution of the original input image
+    :param bool_gaussian_filt: Flag indicating whether to apply a 1d-GaussianFilter (smoothing)
+    to each upsampled patch before fine-tuning the location of each peak.
+    :return: a NUM_JOINTS x 4 np.array where each row represents a joint type (0=nose, 1=neck...)
+    and the columns indicate the {x,y} position, the score (probability) and a unique id (counter)
+    """
+    # MODIFIED BY CARLOS: Instead of upsampling the heatmaps to heatmap_avg and
+    # then performing NMS to find peaks, this step can be sped up by ~25-50x by:
+    # (9-10ms [with GaussFilt] or 5-6ms [without GaussFilt] vs 250-280ms on RoG
+    # 1. Perform NMS at (low-res) CPM's output resolution
+    # 1.1. Find peaks using scipy.ndimage.filters.maximum_filter
+    # 2. Once a peak is found, take a patch of 5x5 centered around the peak, upsample it, and
+    # fine-tune the position of the actual maximum.
+    #  '-> That's equivalent to having found the peak on heatmap_avg, but much faster because we only
+    #      upsample and scan the 5x5 patch instead of the full (e.g.) 480x640
+
+    joint_list_per_joint_type = []
+    cnt_total_joints = 0
+
+    # For every peak found, win_size specifies how many pixels in each
+    # direction from the peak we take to obtain the patch that will be
+    # upsampled. Eg: win_size=1 -> patch is 3x3; win_size=2 -> 5x5
+    # (for BICUBIC interpolation to be accurate, win_size needs to be >=2!)
+    win_size = 2
+
+    for joint in range(NUM_JOINTS):
+        map_orig = heatmaps[:, :, joint]
+        peak_coords = find_peaks(param, map_orig)
+        peaks = np.zeros((len(peak_coords), 4))
+        for i, peak in enumerate(peak_coords):
+            if bool_refine_center:
+                x_min, y_min = np.maximum(0, peak - win_size)
+                x_max, y_max = np.minimum(
+                    np.array(map_orig.T.shape) - 1, peak + win_size)
+
+                # Take a small patch around each peak and only upsample that
+                # tiny region
+                patch = map_orig[y_min:y_max + 1, x_min:x_max + 1]
+                map_upsamp = cv2.resize(
+                    patch, None, fx=upsampFactor, fy=upsampFactor, interpolation=cv2.INTER_CUBIC)
+
+                # Gaussian filtering takes an average of 0.8ms/peak (and there might be
+                # more than one peak per joint!) -> For now, skip it (it's
+                # accurate enough)
+                map_upsamp = gaussian_filter(
+                    map_upsamp, sigma=3) if bool_gaussian_filt else map_upsamp
+
+                # Obtain the coordinates of the maximum value in the patch
+                location_of_max = np.unravel_index(
+                    map_upsamp.argmax(), map_upsamp.shape)
+                # Remember that peaks indicates [x,y] -> need to reverse it for
+                # [y,x]
+                location_of_patch_center = compute_resized_coords(
+                    peak[::-1] - [y_min, x_min], upsampFactor)
+                # Calculate the offset wrt to the patch center where the actual
+                # maximum is
+                refined_center = (location_of_max - location_of_patch_center)
+                peak_score = map_upsamp[location_of_max]
+            else:
+                refined_center = [0, 0]
+                # Flip peak coordinates since they are [x,y] instead of [y,x]
+                peak_score = map_orig[tuple(peak[::-1])]
+            peaks[i, :] = tuple([int(round(x)) for x in compute_resized_coords(
+                peak_coords[i], upsampFactor) + refined_center[::-1]]) + (peak_score, cnt_total_joints)
+            cnt_total_joints += 1
+        joint_list_per_joint_type.append(peaks)
+
+    return joint_list_per_joint_type
+
+
+def get_joint_list(img_orig, param, heatmaps, scale):
+
+    joint_list_per_joint_type = NMS(param,
+                                    heatmaps, img_orig.shape[0] / float(heatmaps.shape[0]))
+
+
+    for peaks in joint_list_per_joint_type:
+        peaks[:, :2] = peaks[:, :2]*scale
+
+    joint_list = np.array([tuple(peak) + (joint_type,) for joint_type, joint_peaks
+                           in enumerate(joint_list_per_joint_type) for peak in joint_peaks])
+
+    return joint_list
+
+
+def draw(canvas, joints, bbox):
+
+
+    x1 = int(bbox[0])
+    y1 = int(bbox[1])
+    x2 = int(bbox[0] + bbox[2])
+    y2 = int(bbox[1] + bbox[3])
+    cv2.rectangle(canvas, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+
+    for i in range(18):
+        if int(joints[i][2]) == 0:
+            continue
+        x = int(joints[i][0])
+        y = int(joints[i][1])
+        cv2.circle(canvas, (x, y), 4, colors[i], thickness=10)
+
+        # cur_canvas = canvas.copy()
+    # stickwidth = 2
+    # for i in range(16):
+    #     if joints[limbSeq[i][0]][2] == 0 or joints[limbSeq[i][1]][2] == 0:
+    #         continue
+    #     X = (int(joints[limbSeq[i][0]][0]), int(joints[limbSeq[i][1]][0]))
+    #     Y = (int(joints[limbSeq[i][0]][1]), int(joints[limbSeq[i][1]][1]))
+    #     mX = np.mean(X)
+    #     mY = np.mean(Y)
+    #     length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
+    #     angle = math.degrees(math.atan2(Y[0] - Y[1], X[0] - X[1]))
+    #     polygon = cv2.ellipse2Poly((int(mX), int(mY)), (int(length / 2), stickwidth), int(angle), 0, 360, 1)
+    #     cv2.fillConvexPoly(canvas, polygon, colors[i])
+
+    return canvas
+
+def plot_result(img_orig, result):
+
+    for idx, person_data in enumerate(result):
+
+        bbox = person_data['bbox']
+        keypoints = person_data['keypoints']
+
+        x = keypoints[0::3]
+        y = keypoints[1::3]
+        v = keypoints[2::3]
+
+        joints = []
+        for i in range(len(x)):
+            joints.append([x[i], y[i], v[i]])
+
+        img_orig = draw(img_orig, joints, bbox)
+    return img_orig
