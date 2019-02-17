1181c872b1b5068fc75e55a27c4bacf323e42677
Igor Muniz <igor.muniz.ims@gmail.com>
2019-02-17 09:58:06 -0300

    change coco order

--- evaluating/multiposenet_evaluate.py
+++ evaluating/multiposenet_evaluate.py
@@ -341,7 +341,7 @@ class CocoEval():
             k = np.zeros(51)
             k[0::3] = bbox_keypoints[i, :, 0]
             k[1::3] = bbox_keypoints[i, :, 1]
-            k[2::3] = [2] * 17
+            k[2::3] = bbox_keypoints[i, :, 2]
 
             pose_score = 0
             count = 0
