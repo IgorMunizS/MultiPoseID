c4a79bfbc24dc0ae5c573b69918d18531dbe0fcd
Igor Muniz <igor.muniz.ims@gmail.com>
2019-03-18 15:15:13 -0300

    intermediate loss training

--- network/keypoint_net.py
+++ network/keypoint_net.py
@@ -25,7 +25,7 @@ class KeypointNet():
         if prediction:
             self.model = Model(inputs=[input_graph], outputs=[self.D])
         else:
-            self.model = Model(inputs=[input_graph], outputs=output_loss)
+            self.model = Model(inputs=[input_graph], outputs=[self.D])
         print(self.model.summary())
 
     def fpn_part(self, C2,C3,C4,C5):
