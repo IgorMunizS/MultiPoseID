cc54ac436d906ed604ccdc483c38a5a3cf0a74c5
Igor Muniz <igor.muniz.ims@gmail.com>
2019-04-02 11:11:16 -0300

    + intermidiate loss

--- network/keypoint_net.py
+++ network/keypoint_net.py
@@ -29,7 +29,7 @@ class KeypointNet():
         self.keypoint_net(P2,P3,P4,P5)
         # self.apply_mask(self.D, input_heat_mask)
 
-        output_loss = [self.D, self.k2[:,:,:,:18], self.k3[:,:,:,:18], self.k4[:,:,:,:18], self.k5[:,:,:,:18]]
+        output_loss = [self.D, self.k2, self.k3, self.k4, self.k5]
 
         if prediction:
             self.model = Model(inputs=[input_graph], outputs=[self.D])
@@ -73,12 +73,12 @@ class KeypointNet():
         # intermidiate supervision for loss
 
         self.k2 = KL.Conv2D(self.nb_keypoints +1, (1,1), strides=1, padding="valid", name='sup_loss_k2') (P2)
-        self.k3 = KL.Conv2D(self.nb_keypoints +1, (1,1), strides=1, padding="valid", name='sup_loss_k3') (P3)
-        self.k3 = KL.UpSampling2D((2, 2), name='sup_loss_k3up')(self.k3)
-        self.k4 = KL.Conv2D(self.nb_keypoints +1, (1, 1), strides=1, padding="valid", name='sup_loss_k4')(P4)
-        self.k4 = KL.UpSampling2D((4, 4), name='sup_loss_k4up')(self.k4)
-        self.k5 = KL.Conv2D(self.nb_keypoints +1, (1, 1), strides=1, padding="valid", name='sup_loss_k5')(P5)
-        self.k5 = KL.UpSampling2D((8, 8), name='sup_loss_k5up')(self.k5)
+        self.k3 = KL.UpSampling2D((2, 2), name='sup_loss_k3up')(P3)
+        self.k3 = KL.Conv2D(self.nb_keypoints +1, (1,1), strides=1, padding="valid", name='sup_loss_k3') (self.k3)
+        self.k4 = KL.UpSampling2D((4, 4), name='sup_loss_k4up')(P4)
+        self.k4 = KL.Conv2D(self.nb_keypoints +1, (1, 1), strides=1, padding="valid", name='sup_loss_k4')(self.k4)
+        self.k5 = KL.UpSampling2D((8, 8), name='sup_loss_k5up')(P5)
+        self.k5 = KL.Conv2D(self.nb_keypoints +1, (1, 1), strides=1, padding="valid", name='sup_loss_k5')(self.k5)
 
 
         self.D2 = KL.Conv2D(128, (3, 3), name="d2_1", padding="same") (P2)
