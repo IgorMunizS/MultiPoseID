5ac96b874ea90758bd81cac230ce3a2655627c1a
Igor Muniz <igor.muniz.ims@gmail.com>
2019-04-02 10:54:22 -0300

    - background

--- network/keypoint_net.py
+++ network/keypoint_net.py
@@ -138,10 +138,10 @@ class KeypointNet():
 
         losses = {}
         losses["Dfinal_2"] = _eucl_loss
-        losses["sup_loss_k2"] = _eucl_loss
-        losses["sup_loss_k3up"] = _eucl_loss
-        losses["sup_loss_k4up"] = _eucl_loss
-        losses["sup_loss_k5up"] = _eucl_loss
+        #losses["sup_loss_k2"] = _eucl_loss
+        #losses["sup_loss_k3up"] = _eucl_loss
+        #losses["sup_loss_k4up"] = _eucl_loss
+        #losses["sup_loss_k5up"] = _eucl_loss
 
         return losses
 
