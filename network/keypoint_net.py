c02faaff25c45305263da9f3a33e128a3604a7c1
Igor Muniz <igor.muniz.ims@gmail.com>
2019-04-30 18:29:38 -0300

    add background

--- network/keypoint_net.py
+++ network/keypoint_net.py
@@ -14,7 +14,7 @@ class KeypointNet():
             input_image = KL.Input(shape=(None, None, 3), name='inputs')
         else:
             input_image = KL.Input(shape=(480, 480, 3), name='inputs')
-        input_heat_mask = KL.Input(shape=(120,120,18), name="mask_heat_input")
+        input_heat_mask = KL.Input(shape=(120,120,19), name="mask_heat_input")
         # backbone = Backbone(input_image, bck_arch, bck_weights).model
         if bck_arch == 'resnet50':
             backbone = keras_resnet.models.FPN2D50(input_image, freeze_bn=False)
